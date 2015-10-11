8cad240b056a717fbea1c0a52d999b1f9009f81e
Duong Vu <duongvu89@gmail.com>
2015-10-11 11:15:09 +0800

    First commit

---
+++ ui-modular/src/main/java/demo/UiApplication.java
@@ -0,0 +1,81 @@
+package demo;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+@SpringBootApplication
+@RestController
+@EnableRedisHttpSession
+public class UiApplication {
+
+	// Match everything without a suffix (so not a static resource)
+	@RequestMapping(value = "/{[path:[^\\.]*}")
+	public String redirect() {
+		// Forward to home page so that route is preserved.
+		return "forward:/";
+	}
+
+	@RequestMapping("/user")
+	@ResponseBody
+	public Principal user(Principal user) {
+		return user;
+	}
+
+/*	@RequestMapping("/resource")
+	@ResponseBody
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}*/
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiApplication.class, args);
+	}
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http.httpBasic().and()
+					.authorizeRequests()
+					.antMatchers("/index.html", "/", "/login", "/message", "/home")
+					.permitAll()
+					.anyRequest().hasRole("USER");
+			// @formatter:on
+		}
+	}
+
+}
